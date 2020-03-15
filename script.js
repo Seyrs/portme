d9df4fb532993d32ac70e715027b195a4681a72f
SeymouR Scott <seyscott5@gmail.com>
2020-03-15 15:17:57 -0400

    edits

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
 
-//Typewriter Effect Quote
-var i = 0;
-var txt = "I've got a theory that if you give 100 percent all of the time, somehow things will work out in the end. -Larry Bird";
-var speed = 40; 
-id = document.getElementById("type").addEventListener("load", typeWriter);
-;
 
-let typeWriter = function() {
-  if (i < txt.length) {
-    id.innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-
-
+setTimeout(function () {
+  $(".typewriter").html("<h3>I've got a theory that if you give 100 percent all of the time, somehow things will work out in the end. -Larry Bird</h3>")
+}, 2500);
